ffbb87571d55e465089a8cc45653edecb1d7071a
Samuel Lucas Rodrigues <samuellucasrdg@gmail.com>
2024-11-21 20:47:13 -0300

    Cria boilerplate da insercao do usuario no banco

--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
   create(createUserDto: CreateUserDto) {
-    return createUserDto;
+    return this.prismaService.user.create({
+      data: {
+        name: createUserDto.name,
+        password: createUserDto.password,
+        email: createUserDto.email,
+      },
+    });
   }
 }
