fa7de5e3c98f717b1f7d4b8a90d8f965d66ed241
Samuel Lucas Rodrigues <samuellucasrdg@gmail.com>
2024-11-21 20:35:01 -0300

    Torna ValidationPipe um GlobalPipe

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -1,19 +1,12 @@
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import {
-  Controller,
-  Post,
-  Body,
-  UsePipes,
-  ValidationPipe,
-} from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  @UsePipes(new ValidationPipe())
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
