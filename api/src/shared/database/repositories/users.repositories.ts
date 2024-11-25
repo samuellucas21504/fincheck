25690872c23fdfa86e6c2ab4bd7ccea4b30d345f
Samuel Lucas Rodrigues <samuellucasrdg@gmail.com>
2024-11-25 20:44:56 -0300

    Implementa usuario
    - Cria UserRepository, UserService

---
+++ api/src/shared/database/repositories/users.repositories.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { type Prisma } from '@prisma/client';
+
+@Injectable()
+export class UserRepository {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  create(createDto: Prisma.UserCreateArgs) {
+    return this.prismaService.user.create(createDto);
+  }
+
+  findUnique(findUnique: Prisma.UserFindUniqueArgs) {
+    return this.prismaService.user.findUnique(findUnique);
+  }
+}
